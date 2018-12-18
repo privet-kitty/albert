e63839e5413c73e8d8ef835386487d98c71adf7a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-18 05:11:07 +0000

    Deploy privet-kitty/albert to github.com/privet-kitty/albert.git:gh-pages

---
+++ src/colorspace.js
@@ -0,0 +1,152 @@
+import {TWO_PI,
+        mod,
+        multMatrixVector,
+        multMatrixMatrix,
+        clamp} from './arithmetic.js';
+
+const CONST1 = 216/24389;
+const CONST2 = 24389/27/116;
+const CONST3 = 16/116;
+
+export function functionF(x) {
+  // used in XYZ -> Lab conversion
+  if (x > CONST1) {
+    return Math.pow(x, 0.3333333333333333);
+  } else {
+    return CONST2 * x + CONST3;
+  }
+}
+
+export function calcLabToLCHab(astar, bstar) {
+  // Omits L*.
+  return [Math.sqrt((astar*astar) + (bstar*bstar)),
+          mod((Math.atan2(bstar, astar) / TWO_PI) * 360, 360)];
+}
+
+export function calcLCHabToLab(cstarab, hab) {
+  // Omits L*.
+  const hue2PI = (hab / 360) * TWO_PI;
+  return [cstarab * Math.cos(hue2PI), cstarab * Math.sin(hue2PI)];
+}
+
+
+class Illuminant {
+  constructor(X, Z, catMatrixCToThis, catMatrixThisToC) {
+    this.X = X;
+    this.Z = Z;
+    this.catMatrixCToThis = catMatrixCToThis;
+    this.catMatrixThisToC = catMatrixThisToC;
+  }
+}
+
+// The data are based on dufy.
+export const ILLUMINANT_D65 =
+  new Illuminant(0.950428061568676,
+                 1.08891545904089,
+                 [[0.9904112147597705,-0.00718628493839008,-0.011587161829988951],
+                  [-0.012395677058354078,1.01560663662526,-0.0029181533414322086],
+                  [-0.003558889496942143,0.006762494889396557,0.9182865019746504]],
+                 [[1.0098158523233767,0.007060316533713093,0.012764537821734395],
+                  [0.012335983421444891,0.9846986027789835,0.003284857773421468],
+                  [0.003822773174044815,-0.007224207660971385,1.0890100329203007]]);
+export const ILLUMINANT_C =
+  new Illuminant(0.9807171421603395,
+                 1.182248923134197,
+                 [[1,0,0],[0,1,0],[0,0,1]],
+                 [[1,0,0],[0,1,0],[0,0,1]]);
+
+const DELTA = 6/29;
+const CONST4 = 3*DELTA*DELTA;
+
+export function calcLabToXYZ(lstar, astar, bstar, illuminant = ILLUMINANT_D65) {
+  const fy = (lstar + 16) / 116;
+  const fx = fy + astar * 0.002;
+  const fz = fy - bstar * 0.005;
+  const Xw = illuminant.X;
+  const Zw = illuminant.Z;
+  return [ (fx > DELTA) ? (fx*fx*fx*Xw) : ((fx - CONST3) * CONST4 * Xw),
+           (fy > DELTA) ? (fy*fy*fy) : ((fy - CONST3) * CONST4),
+           (fz > DELTA) ? (fz*fz*fz*Zw) : ((fz - CONST3) * CONST4 * Zw) ];  
+}
+
+function genLinearizer(gamma) {
+  // Returns a function for gamma-correction (not for sRGB).
+  const reciprocal = 1/gamma;
+  return (x) => {
+    return x >= 0 ? Math.pow(x, reciprocal) : -Math.pow(-x, reciprocal);
+  };
+}
+
+function genDelinearizer(gamma) {
+  // Returns a function for linearization (not for sRGB).
+  return (x) => {
+    return x >= 0 ? Math.pow(x, gamma) : -Math.pow(-x, gamma);
+  };
+}
+
+class RGBSpace {
+  constructor(matrixThisToXYZ, matrixXYZFromThis,
+              linearizer = genLinearizer(2.2), delinearizer = genDelinearizer(2.2),
+              illuminant = ILLUMINANT_D65) {
+    this.matrixThisToXYZ = matrixThisToXYZ;
+    this.matrixXYZFromThis = matrixXYZFromThis;
+    this.linearizer = linearizer;
+    this.delinearizer = delinearizer;
+    this.illuminant = illuminant;
+  }
+}
+
+const CONST5 = 0.0031308*12.92;
+
+export const SRGB = new RGBSpace(
+  // These data are based on dufy.
+  [[0.4124319639872968,0.3575780371782625,0.1804592355313134],
+   [0.21266023143094992,0.715156074356525,0.07218369421252536],
+   [0.01933274831190452,0.11919267905942081,0.9504186404649174]],
+  [[3.240646461582504,-1.537229731776316,-0.49856099408961585],
+   [-0.969260718909152,1.876000564872059,0.04155578980259398],
+   [0.05563672378977863,-0.2040013205625215,1.0570977520057931]],
+  (x) => {
+    if (x > CONST5) {
+      return Math.pow ((0.055 + x) / 1.055, 2.4);
+    } else if (x < -CONST5) {
+      return - Math.pow ((0.055 - 1) / 1.055, 2.4);
+    } else {
+      return x / 12.92;
+    }
+  },
+  (x) => {
+    if (x > 0.0031308) {
+      return Math.pow(x, 1/2.4) * 1.055 - 0.055;
+    } else if (x < -0.0031308) {
+      return - Math.pow(-x, 1/2.4) * 1.055 + 0.055;
+    } else {
+      return x * 12.92;
+    }
+  }
+);
+
+export function calcXYZToLinearRGB(X, Y, Z, space = SRGB) {
+  return multMatrixVector(space.matrixXYZFromThis, [X, Y, Z]);
+}
+
+export function calcLinearRGBToRGB(lr, lg, lb, space = SRGB) {
+  return [lr, lg, lb].map(space.delinearizer);
+}
+
+
+export function calcRGBToRGB255(r, g, b, clamp = true) {
+  if (clamp) {
+    return [r, g, b].map((x) => Math.max(Math.min(Math.round(x * 255), 255), 0));
+  } else {
+    return [r, g, b].map((x) => Math.round(x * 255));
+  }
+}
+
+export function calcRGBToHex(r, g, b) {
+  return "#" + [r, g, b].map((x) => {
+    const hex = clamp(Math.round(x * 255), 0, 255).toString(16);
+    return hex.length === 1 ? "0" + hex : hex;
+  }).join("");
+}
+
---
+++ test/colorspace.test.js
@@ -0,0 +1,55 @@
+import {calcLCHabToLab,
+        calcLabToLCHab,
+        calcLabToXYZ,
+        calcXYZToLinearRGB,
+        calcLinearRGBToRGB,
+        calcRGBToRGB255,
+        calcRGBToHex,
+        ILLUMINANT_C} from '../src/colorspace.js';
+import './jest-extension.js';
+
+describe('Lab <-> LCHab', () => {
+  test('boundary case', () => {
+    expect(calcLCHabToLab(1, 360)).toNearlyEqual([1, 0], 10);
+    expect(calcLCHabToLab(2, -90)).toNearlyEqual([0, -2], 10);
+  })
+  test('round-trip', () => {
+    for (let ab of [[-3, 4], [3.9e10, 3.9e-10], [0, 0]]) {
+      expect(calcLCHabToLab.apply(null, calcLabToLCHab.apply(null, ab))).toNearlyEqual(ab, 10);
+    }
+  })
+})
+
+describe('Lab <-> XYZ', () => {
+  test('boundary case', () => {
+    expect(calcLabToXYZ(100, 0, 0, ILLUMINANT_C)).toNearlyEqual([ILLUMINANT_C.X, 1, ILLUMINANT_C.Z], 5);
+  })
+  test('consistency with dufy', () => {
+    expect(calcLabToXYZ(1, 1, 1)).toNearlyEqual([0.0012962827110591009, 0.0011070564598794526, 5.063061751414016e-4], 6);
+    expect(calcLabToXYZ(50, 10, 3)).toNearlyEqual([0.19417300681355418, 0.18418651851244416, 0.1851153188341182],6);
+  })
+})
+
+describe('XYZ <-> liner RGB', () => {
+  test('consistency with dufy (sRGB)', () => {
+    expect(calcXYZToLinearRGB(0.1, 0.2, 0.3)).toNearlyEqual([-0.1329496041195698, 0.29074078266626474, 0.28189274594473257], 5);
+  })
+})
+
+describe('linear RGB <-> gamma-corrected RGB', () => {
+  test('consistency with dufy (sRGB)', () => {
+    expect(calcLinearRGBToRGB(0.002, 0.7, -0.2)).toNearlyEqual([0.025840000000000002, 0.85430583154494, -0.48452920448170694], 6);
+  })
+})
+
+describe('RGB <-> quantized RGB', () => {
+  test('clamp check', () => {
+    expect(calcRGBToRGB255(0.101, 1.1, -0.3)).toEqual([26, 255, 0]);
+  })
+})
+
+describe('RGB <-> Hex code', () => {
+  test('clamp check', () => {
+    expect(calcRGBToHex(0.101, 1.1, -0.3)).toEqual("#1aff00");
+  })
+})
