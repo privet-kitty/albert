e3045d4c0537cf5780c91ee57f43fd555082bfa5
Hugo Sansaqua <privet.kitty99@gmail.com>
2024-03-11 09:41:01 +0900

    Fix prod build

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 
-const isStatic = process.env.ALBERT_STATIC === "true";
+const isProd = process.env.ALBERT_ENV === "production";
+const isStatic = isProd || process.env.ALBERT_STATIC === "true";
+
+const PROD_SUBDIRECTORY = "/albert";
 
 const nextConfig = {
   output: isStatic ? "export" : "standalone",
+  basePath: isProd ? PROD_SUBDIRECTORY : "",
+  assetPrefix: isProd ? PROD_SUBDIRECTORY : "",
 };
 
 export default nextConfig;
